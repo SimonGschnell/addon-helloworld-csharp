19fff001dc34730119a974b96b2817f34342314e
Simon Gschnell <simongschnell@gmail.com>
2026-02-28 22:28:10 +0100

    adds immediate job trigger and everyday 8am job trigger

--- StremioAddonExample/ResourceFetcher/Program.cs
+++ StremioAddonExample/ResourceFetcher/Program.cs
@@ -24,11 +24,18 @@ public class Program
         {
             var netflixJob = new JobKey("NetflixFetch");
             q.AddJob<NetflixFetch>(opts => opts.WithIdentity(netflixJob));
+
+            q.AddTrigger(opts => opts
+                .ForJob(netflixJob)
+                .WithIdentity("NetflixFetchTriggerNow")
+                .StartNow());
             q.AddTrigger(opts => opts
                 .ForJob(netflixJob)
                 .WithIdentity("NetflixFetchTrigger")
-                .StartNow()
-                .WithSimpleSchedule(x => x.WithInterval(TimeSpan.FromMinutes(1)).RepeatForever()));
+                .WithCronSchedule("0 0 8 ? * *", x =>
+                {
+                    x.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Europe/Vienna"));
+                }));
         });
 
         builder.Services.AddQuartzHostedService(opts => { opts.WaitForJobsToComplete = true; });
