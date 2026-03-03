51dc54be567cc2177eb67cc14e1d0bd524e05726
Simon Gschnell <simongschnell@gmail.com>
2026-03-03 19:17:23 +0100

    creates own service for persistence to make Fetch method testable

---
+++ StremioAddonExample/ResourceFetcher/Services/IPersistenceService.cs
@@ -0,0 +1,24 @@
+using ResourceFetcher.Helpers;
+using StremioAddonExample.Models;
+
+namespace ResourceFetcher.Services;
+
+public interface IPersistenceService
+{
+    public Task PersistCatalogMetaData(CatalogType catalogType, CatalogId catalogId, string data);
+}
+
+public class FilePersistence : IPersistenceService
+{
+    public async Task PersistCatalogMetaData(CatalogType catalogType, CatalogId catalogId, string data)
+    {
+        var directoryPath = Path.Combine(EnvironmentHelper.GetOutputPath(), catalogType.ToString());
+
+        var filePath = Path.Combine(directoryPath, $"{catalogId.ToString()}.json");
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        await File.WriteAllTextAsync(filePath, data);
+    }
+}
