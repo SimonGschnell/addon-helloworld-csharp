b5c7af76602aa1d23d9f7e86a40eab1690fcc16e
Simon Gschnell <simongschnell@gmail.com>
2026-03-03 19:18:15 +0100

    Decouple responsability of Cron job and actual service fetch orchestration

--- StremioAddonExample/ResourceFetcher/CronJobs/FetchShowsJob.cs
+++ StremioAddonExample/ResourceFetcher/CronJobs/FetchShowsJob.cs
@@ -1,80 +1,30 @@
-using Newtonsoft.Json;
 using Quartz;
-using ResourceFetcher.Helpers;
-using ResourceFetcher.Models;
 using ResourceFetcher.Services;
-using StremioAddonExample.Models;
 
 namespace ResourceFetcher.CronJobs;
 
 public class FetchShowsJob: IJob
 {
+    private readonly FetchServiceOrchestrator _fetchServiceOrchestrator;
     private readonly IFetchServiceCollection _fetchServiceCollection;
     private readonly ILogger<FetchShowsJob> _logger;
     private readonly ResourceFetcherHttpClient _client;
 
-    public FetchShowsJob(ILogger<FetchShowsJob> logger, ResourceFetcherHttpClient client, IFetchServiceCollection fetchServiceCollection)
+    public FetchShowsJob(ILogger<FetchShowsJob> logger, FetchServiceOrchestrator fetchServiceOrchestrator)
     {
-        _fetchServiceCollection = fetchServiceCollection;
+        _fetchServiceOrchestrator = fetchServiceOrchestrator;
         _logger = logger;
-        _client = client;
     }
     
     public async Task Execute(IJobExecutionContext context)
     {
         try
         {
-            await FetchServicesAsync();
+            await _fetchServiceOrchestrator.FetchServicesAsync();
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, ex.Message);
         }
-    }
-
-    private async Task FetchServicesAsync()
-    {
-        _logger.LogInformation("Fetching shows: {time}", DateTime.UtcNow);
-        foreach (var fetchService in _fetchServiceCollection.Services)
-        {
-            await FetchAndPersistFor(CatalogType.movie, fetchService);
-            await FetchAndPersistFor(CatalogType.series, fetchService);
-        }
-    }
-
-    private async Task FetchAndPersistFor(CatalogType catalogType, IFetchService fetchService)
-    {
-        var result = await GetApiResultFor(catalogType, fetchService);
-        var metadata = ConvertApiResultToMetaData(result, fetchService);
-        await PersistCatalogMetaData(catalogType, fetchService.CatalogId, metadata);
-    }
-
-    private async Task<string> GetApiResultFor(CatalogType type, IFetchService fetchService)
-    {
-        _logger.LogInformation("Fetching {catalogType} for {service}: {time}", type.ToString(), fetchService.Name, DateTime.UtcNow);
-        var request = fetchService.GetRequest(type);
-        return await _client.FetchAsync(request);
-    }
-
-    private static string ConvertApiResultToMetaData(string movieResponse, IFetchService fetchService)
-    {
-        var metasList = fetchService.ConvertToMetaData(movieResponse);
-
-        return JsonConvert.SerializeObject(new CatalogModel
-        {
-            Metas = metasList.ToArray(),
-        });
-    }
-
-    private static async Task PersistCatalogMetaData(CatalogType catalogType, CatalogId catalogId, string data)
-    {
-        var directoryPath = Path.Combine(EnvironmentHelper.GetOutputPath(), catalogType.ToString());
-
-        var filePath = Path.Combine(directoryPath, $"{catalogId.ToString()}.json");
-        if (!Directory.Exists(directoryPath))
-        {
-            Directory.CreateDirectory(directoryPath);
-        }
-        await File.WriteAllTextAsync(filePath, data);
     }
 }
---
+++ StremioAddonExample/ResourceFetcher/Services/FetchServiceOrchestrator.cs
@@ -0,0 +1,57 @@
+using Newtonsoft.Json;
+using ResourceFetcher.Helpers;
+using StremioAddonExample.Models;
+
+namespace ResourceFetcher.Services;
+
+public class FetchServiceOrchestrator
+{
+    private readonly ILogger<FetchServiceOrchestrator> _logger;
+    private readonly IResourceFetcherHttpClient _client;
+    private readonly IFetchServiceCollection _fetchServiceCollection;
+    private readonly IPersistenceService _persistenceService;
+
+    public FetchServiceOrchestrator(ILogger<FetchServiceOrchestrator> logger, IResourceFetcherHttpClient client, IFetchServiceCollection fetchServiceCollection, IPersistenceService persistenceService )
+    {
+        _logger = logger;
+        _client = client;
+        _fetchServiceCollection = fetchServiceCollection;
+        _persistenceService = persistenceService;
+    }
+    
+    public async Task FetchServicesAsync()
+    {
+        _logger.LogInformation("Fetching shows: {time}", DateTime.UtcNow);
+        foreach (var fetchService in _fetchServiceCollection.Services)
+        {
+            await FetchAndPersistFor(CatalogType.movie, fetchService);
+            await FetchAndPersistFor(CatalogType.series, fetchService);
+        }
+    }
+    
+    private async Task FetchAndPersistFor(CatalogType catalogType, IFetchService fetchService)
+    {
+        var result = await GetApiResultFor(catalogType, fetchService);
+        var metadata = ConvertApiResultToMetaData(result, fetchService);
+        await _persistenceService.PersistCatalogMetaData(catalogType, fetchService.CatalogId, metadata);
+    }
+
+    private async Task<string> GetApiResultFor(CatalogType type, IFetchService fetchService)
+    {
+        _logger.LogInformation("Fetching {catalogType} for {service}: {time}", type.ToString(), fetchService.Name, DateTime.UtcNow);
+        var request = fetchService.GetRequest(type);
+        return await _client.FetchAsync(request);
+    }
+
+    private static string ConvertApiResultToMetaData(string movieResponse, IFetchService fetchService)
+    {
+        var metasList = fetchService.ConvertToMetaData(movieResponse);
+
+        return JsonConvert.SerializeObject(new CatalogModel
+        {
+            Metas = metasList.ToArray(),
+        });
+    }
+
+    
+}
